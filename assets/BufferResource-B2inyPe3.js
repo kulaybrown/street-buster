89d214c638df8f636ece3dc450a6e572164cbd86
Angelo <iamthelosworld@gmail.com>
2026-06-06 03:38:32 +0800

    Updates

--- assets/BufferResource-B2inyPe3.js
+++ assets/BufferResource-B2inyPe3.js
@@ -1,4 +1,4 @@
-import{an as U,a7 as g,S as c,al as S,c as _,d as m,E as I,ak as p}from"./index-Mdc2C7-0.js";const x={name:"local-uniform-bit",vertex:{header:`
+import{an as U,a7 as g,S as c,al as S,c as _,d as m,E as I,ak as p}from"./index-DgFl6ABR.js";const x={name:"local-uniform-bit",vertex:{header:`
 
             struct LocalUniforms {
                 uTransformMatrix:mat3x3<f32>,
